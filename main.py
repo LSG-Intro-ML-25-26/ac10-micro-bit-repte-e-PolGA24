f7e3a1c66ada917a5b360908fe145022320aa297
Pol González Arribas <polgonzarri@gmail.com>
2025-12-10 08:30:21 +0100

    moure gota per la pantalla

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
+x = 2
+y = 2
 
+while True:
+    led.plot(x, y)
+    basic.pause(50)
+    led.unplot(x, y)
+
+    accx = input.acceleration(Dimension.X)
+    accy = input.acceleration(Dimension.Y)
+
+    if accx <= -150 and x > 0:
+        x = x - 1
+    if accx > 150 and x < 4:
+        x = x + 1
+        
+    if accy <= -150 and y > 0:
+        y = y - 1
+    if accy > 150 and y < 4:
+        y = y + 1
--- main.ts
+++ main.ts
@@ -1,1 +1,27 @@
-
+let accx: number;
+let accy: number;
+let x = 2
+let y = 2
+while (true) {
+    led.plot(x, y)
+    basic.pause(50)
+    led.unplot(x, y)
+    accx = input.acceleration(Dimension.X)
+    accy = input.acceleration(Dimension.Y)
+    if (accx <= -150 && x > 0) {
+        x = x - 1
+    }
+    
+    if (accx > 150 && x < 4) {
+        x = x + 1
+    }
+    
+    if (accy <= -150 && y > 0) {
+        y = y - 1
+    }
+    
+    if (accy > 150 && y < 4) {
+        y = y + 1
+    }
+    
+}
